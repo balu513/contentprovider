80e8fd72f6c7e7ada2692cbca7c046350970e467
balakrishna <balub513@gmail.com>
2015-08-10 16:40:34 +0530

    comm

---
+++ app/src/main/java/com/afbb/balakrishna/albumart/widget/MyWidgetProvider.java
@@ -0,0 +1,36 @@
+package com.afbb.balakrishna.albumart.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import com.afbb.balakrishna.albumart.R;
+import com.afbb.balakrishna.albumart.activities.WidgetActivty;
+
+import java.util.Random;
+
+public class MyWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        Toast.makeText(context, "onReceive", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        int N = appWidgetIds.length;
+        for (int i = 0; i < N; i++) {
+            Intent intent = new Intent(context, WidgetActivty.class);
+            PendingIntent.getActivity(context, 100, intent, 0);
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widgetlayout);
+            remoteViews.setTextViewText(R.id.textView_widget, new Random().nextInt(1000) + "");
+            appWidgetManager.updateAppWidget(appWidgetIds[i], remoteViews);
+        }
+    }
+}
